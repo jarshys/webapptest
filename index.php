bff6d767730eff9467090551ea9eeb4223c37939
Luis Romero <luromer@microsoft.com>
2018-08-30 13:55:45 -0500

    changes to forms 5

--- index.php
+++ index.php
@@ -69,20 +69,20 @@ The PHP script insertToDb.php will be executed after the user clicks "Submit"-->
 	<form action="insertToDb.php" method="post">
 
 		<label>Vehicle Make:</label>
-		<input type="string" step="1" name="vehicleMake" required><br>
+		<input type="text" step="1" name="vehicleMake" required><br>
 		<br>
 		<label>Vehicle Model:</label>
-		<input type="string" step="1" name="vehicleModel" required><br>
+		<input type="text" step="1" name="vehicleModel" required><br>
 		<br>
 		<!-- Text input for the day start and end -->
 		<label>Start Date (MMDDYY):</label>
 		<input type="number" step="1" name="startDay" required><br>
 		<br>
 		<label>End Date (MMDDYY):</label>
-		<input type="number" step="0.01" name="endDay" required><br>
+		<input type="number" step="1" name="endDay" required><br>
 		<br>
 		<label>Employee Name:</label>
-		<input type="string" step="1" name="employeeName" required>
+		<input type="text" step="1" name="employeeName" required>
 		<br>
 		<button type="submit">Submit</button>
 	</form>
